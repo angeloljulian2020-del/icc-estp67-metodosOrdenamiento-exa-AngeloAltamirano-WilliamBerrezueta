cfb92548d9ac936e563e5a8ef36d1a9656309dd2
Your Name <you@example.com>
2026-05-20 14:01:24 +0000

    frist commit

--- src/controllers/BrandController.java
+++ src/controllers/BrandController.java
@@ -1,5 +1,28 @@
 package controllers;
 
+import models.Brand;
+
 public class BrandController {
+  public Brand[] sortBubbleDesc(Brand[] brands) {
+    // Implementación del método
+    for (int i = 0; i < brands.length - 1; i++) {
+      boolean swap = false;
+      for (int j = 0; j < brands.length - i - 1; j++) {
+        if (brands[i].getTotalValidYears() < brands[j + 1].getTotalValidYears()) {
+          Brand aux = brands[j];
+          brands[j] = brands[j + 1];
+          brands[j + 1] = aux;
+          swap = true;
+        }
+
+      }
+      if (!swap) {
+        break;
+      }
+
+    }
+    return brands;
+
+  }
 
 }
--- src/models/Brand.java
+++ src/models/Brand.java
@@ -1,5 +1,51 @@
 package models;
 
+import java.util.Arrays;
+
 public class Brand {
+
+  public int getTotalValidYears() {
+    // Implementar aquí
+    int total = 0;
+    for (CarModel model : models) {
+      for (CarYear year : model.getYears()) {
+        if (year.isValid()) {
+          total += 1;
+        }
+      }
+
+    }
+    return total;
+
+  }
+
+  private String brandName;
+  private CarModel[] models;
+
+  public String getBrandName() {
+    return brandName;
+  }
+
+  public void setBrandName(String brandName) {
+    this.brandName = brandName;
+  }
+
+  public CarModel[] getModels() {
+    return models;
+  }
+
+  public void setModels(CarModel[] models) {
+    this.models = models;
+  }
+
+  public Brand(String brandName, CarModel[] models) {
+    this.brandName = brandName;
+    this.models = models;
+  }
+
+  @Override
+  public String toString() {
+    return "Brand [brandName=" + brandName + ", models=" + Arrays.toString(models) + "]";
+  }
 
 }
